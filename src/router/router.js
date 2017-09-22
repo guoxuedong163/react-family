91f5633513b9d438e37cb942853792476736763b
郭学东 <guoxuedong163@163.com>
2017-09-22 18:29:39 +0800

    改web pack

--- src/router/router.js
+++ src/router/router.js
@@ -1,25 +1,38 @@
 import React from 'react';
 
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import Home from 'pages/home/home';
-import Page1 from 'pages/page1/page1';
-import Counter from 'pages/counter/counter';
-import UserInfo from 'pages/userinfo/userinfo';
+import Bundle from './bundle';
+import Home from 'bundle-loader?lazy&name=home!pages/home/home';
+import Page1 from 'bundle-loader?lazy&name=page1!pages/page1/page1';
+import Counter from 'bundle-loader?lazy&name=counter!pages/counter/counter';
+import UserInfo from 'bundle-loader?lazy&name=userinfo!pages/userinfo/userinfo';
+
+const Loading = function () {
+    return <div>loading...</div>
+}
+
+const createComponent = (component) => () => (
+    <Bundle load={component}>
+        {
+            (Component) => Component ? <Component /> : <Loading />
+        }
+    </Bundle>
+)
 
 export default () => (
     <Router>
         <div>
             <ul>
                 <li><Link to='/'>首页</Link></li>
-                <li><Link to='/page1'>Page1</Link></li>
+                <li><Link to='/page1'>Page1 </Link></li>
                 <li><Link to='/counter'>Counter</Link></li>
                 <li><Link to='/userinfo'>UserInfo</Link></li>
             </ul>
             <Switch>
-                <Route exact path='/' component={Home}></Route>
-                <Route path='/page1' component={Page1}></Route>
-                <Route path='/counter' component={Counter}></Route>
-                <Route path='/userinfo' component={UserInfo}></Route>
+                <Route exact path='/' component={createComponent(Home)}></Route>
+                <Route path='/page1' component={createComponent(Page1)}></Route>
+                <Route path='/counter' component={createComponent(Counter)}></Route>
+                <Route path='/userinfo' component={createComponent(UserInfo)}></Route>
             </Switch>
         </div>
     </Router>
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,48 +1,36 @@
 const path = require('path');
+const merge = require('webpack-merge');
+const commonConfig = require('./webpack.common.config.js');
 
-module.exports = {
-    entry: [
-        path.join(__dirname, 'src/index.js')
-    ],
+const devConfig = {
+    devtool: 'inline-source-map',
+    entry: {
+        app: [path.join(__dirname, 'src/index.js')]
+    },
     output: {
-        path: path.join(__dirname, './dist'),
-        filename: 'bundle.js'
+        filename: '[name].[hash].js'
     },
     module: {
         rules: [
             {
-                test: /\.js$/,
-                use: ['babel-loader?cacheDirectory=true'],
-                include: path.join(__dirname, 'src')
-            },
-            {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(png|jpg|gif)$/,
-                use: [{
-                    loader: 'url-loader',
-                    options: {
-                        limit: 8192
-                    }
-                }]
+                use: ["style-loader", 'css-loader']
             }
         ]
     },
-    devtool: 'inline-source-map',
     devServer: {
         contentBase: path.join(__dirname, './dist'),
-        port: '5500',
-        host: '127.0.0.1'
-    },
-    resolve: {
-        alias: {
-            pages: path.join(__dirname, 'src/pages'),
-            component: path.join(__dirname, 'src/component'),
-            router: path.join(__dirname, 'src/router'),
-            actions: path.join(__dirname, 'src/redux/actions'),
-            reducers: path.join(__dirname, 'src/redux/reducers')
-        }
+        historyApiFallback: true,
+        host: '0.0.0.0',
+        port: '5500'
     }
 }
+
+module.exports = merge({
+    customizeArray(a, b, key) {
+        if (key === 'entry.app') {
+            return b;
+        }
+        return undefined;
+    }
+})(commonConfig, devConfig);
